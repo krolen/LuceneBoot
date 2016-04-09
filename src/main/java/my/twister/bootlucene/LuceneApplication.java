779dbb79d3402009794d7c49ef6f5b313eedce0e
kkulagin_sysomos <kkulagin@sysomos.com>
2016-04-09 00:31:38 -0400

    number of threads decreased

--- src/main/java/my/twister/bootlucene/LuceneApplication.java
+++ src/main/java/my/twister/bootlucene/LuceneApplication.java
@@ -1,7 +1,13 @@
 package my.twister.bootlucene;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.jetty.JettyServerCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -11,6 +17,25 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 //@IntegrationComponentScan
 //@EnableWebSocket
 public class LuceneApplication {
+
+  @Bean
+  public JettyEmbeddedServletContainerFactory jettyEmbeddedServletContainerFactory(@Value("${jetty.threadPool.maxThreads:50}") final String maxThreads,
+                                                                                   @Value("${jetty.threadPool.minThreads:10}") final String minThreads,
+                                                                                   @Value("${jetty.threadPool.idleTimeout:60000}") final String idleTimeout) {
+    Integer port = Integer.valueOf(System.getProperty("server.port"));
+    final JettyEmbeddedServletContainerFactory factory = new JettyEmbeddedServletContainerFactory(port);
+    factory.addServerCustomizers(new JettyServerCustomizer() {
+      @Override
+      public void customize(final Server server) {
+        // Tweak the connection pool used by Jetty to handle incoming HTTP connections
+        final QueuedThreadPool threadPool = server.getBean(QueuedThreadPool.class);
+        threadPool.setMaxThreads(Integer.valueOf(maxThreads));
+        threadPool.setMinThreads(Integer.valueOf(minThreads));
+        threadPool.setIdleTimeout(Integer.valueOf(idleTimeout));
+      }
+    });
+    return factory;
+  }
 
   public static void main(String[] args) {
     int thisAppNumber = Integer.parseInt(System.getProperty("thisAppNumber"));
